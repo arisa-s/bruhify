7fbc5c0f9772d7ac511945bdb41210d7273f8ece
NBAtbaldwin <tzadoroznybaldwin@gmail.com>
2021-09-05 17:05:13 -0400

    run script less often by default

--- public/bruhify.js
+++ public/bruhify.js
@@ -1,15 +1,32 @@
+const DEFAULT_ELEMENT_THRESHOLD = 0.05;
+const DEFAULT_WORD_THRESHOLD = 0.01;
 
+const getSafeThreshold = (argThreshold) =>
+  argThreshold > 1 || argThreshold < 0
+    ? DEFAULT_ELEMENT_THRESHOLD
+    : argThreshold;
 
-const bruhify = (threshold = .03) => {
-  const safeThreshold = threshold > 1 || threshold < 0 ? .5 : threshold
+const bruhify = ({
+  elementThreshold = DEFAULT_ELEMENT_THRESHOLD,
+  wordThreshold = DEFAULT_WORD_THRESHOLD,
+}) => {
+  const safeElementThreshold = getSafeThreshold(elementThreshold);
+  const safeWordThreshold = getSafeThreshold(wordThreshold);
 
   const [DEBOUNCE_DELAY, TARGET_ELEMENT, TEXT] = [2000, "body", "bruh"];
 
   const targetNode = document.querySelector(TARGET_ELEMENT);
 
-  const shouldBruh = () => Math.random() < safeThreshold
+  const shouldBruh = (threshold) => Math.random() < threshold;
 
-  const addBruh = (htmlElement) => htmlElement.textContent.split(" ").map(word => shouldBruh() ? TEXT : word).join(" ")
+  const addBruh = (htmlElement) => {
+    if (shouldBruh(safeElementThreshold)) {
+      htmlElement.textContent = htmlElement.textContent
+        .split(" ")
+        .map((word) => (shouldBruh(safeWordThreshold) ? TEXT : word))
+        .join(" ");
+    }
+  };
 
   const debounce = (fn, wait) => {
     let timer;
@@ -25,26 +42,31 @@ const bruhify = (threshold = .03) => {
     const len = element.childNodes.length;
 
     if (len === 0) {
-      if (!element.textContent) return
+      if (!element.textContent) return;
 
       // Element is textnode
       if (element.nodeType === 3) {
-        if (!element.parentElement || !element.parentElement.dataset || element.parentElement.dataset.isBruhed) return
+        if (
+          !element.parentElement ||
+          !element.parentElement.dataset ||
+          element.parentElement.dataset.isBruhed
+        )
+          return;
 
-        element.parentElement.dataset.isBruhed = true
+        element.parentElement.dataset.isBruhed = true;
 
-        element.textContent = addBruh(element)
+        addBruh(element);
 
-        return
+        return;
       }
 
-      if (element.dataset && element.dataset.isBruhed) return
+      if (element.dataset && element.dataset.isBruhed) return;
 
       if (element.dataset) {
-        element.dataset.isBruhed = true
+        element.dataset.isBruhed = true;
       }
 
-      element.textContent = addBruh(element)
+      addBruh(element);
 
       return;
     }
@@ -71,6 +93,6 @@ const bruhify = (threshold = .03) => {
     childList: true,
     subtree: true,
   });
-}
+};
 
-bruhify()
+bruhify({});
