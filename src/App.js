5300ecb6e06bc1743789a82831094e0b4f8b28fc
arisa-s <arisashiraishi@outlook.com>
2021-09-05 17:10:22 -0400

    felt cute, might delete later

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import logo from './logo.png';
 import './App.css';
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { purple } from '@material-ui/core/colors';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+
+const PurpleSwitch = withStyles({
+  switchBase: {
+    color: purple[300],
+    '&$checked': {
+      color: purple[500],
+    },
+    '&$checked + $track': {
+      backgroundColor: purple[500],
+    },
+  },
+  checked: {},
+  track: {},
+})(Switch);
 
 function App() {
-  const [isBruh, setIsBruh] = React.useState("false")
+  const [isBruh, setIsBruh] = React.useState(false)
   return (
     <div className="App">
-      <h3> Bruhify contents </h3>
-      <label className="switch">
-        <input typeName="checkbox"/>
-        <span className="slider round"></span>
-        </label>
-     
+        <FormControlLabel
+        control={<PurpleSwitch checked={isBruh} onChange={() => setIsBruh(!isBruh)} name="bruhSwitch" />}
+        label="Bruhify contents"
+      />
     </div>
   );
 }
 
 export default App;
+
